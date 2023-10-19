2e7a473ce2023e5025783b49fb0014608ad82804
velesnic <leslieeevega@gmail.com>
2023-10-19 15:06:31 -0500

    new branch main menu

--- week3project/src/main/java/org/example/Main.java
+++ week3project/src/main/java/org/example/Main.java
@@ -30,7 +30,7 @@ public class Main {
                 }
 
 
-                isMakingSelection = false;
+                isMakingSelection = false; 
             }
             catch(Exception ex){
 
