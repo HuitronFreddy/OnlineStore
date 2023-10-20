dac61eff7e062f73d0c0ceb6289bc54282cf5f8c
Theo Adodoadji <theophilinea@gmail.com>
2023-10-20 13:17:09 -0500

    theo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,19 +17,6 @@ public class Main {
 
             ArrayList<Product> inventoryList = new ArrayList<>();
 
-            inventoryList.add(new Product("AV1051", "JBL Bluetooth Speaker", 89.95, "Audio Video"));
-            inventoryList.add(new Product("AV1312", "Mini 1000 Lumens Projector", 149.95, "Audio Video"));
-            inventoryList.add(new Product("AV1412", "XLR Podcast Cardiod Mic", 44.99, "Audio Video"));
-            inventoryList.add(new Product("CP2012", "Desktop PC Computer Intel Core i5", 139.00, "Computers"));
-            inventoryList.add(new Product("CP2123", "Wired Backlit Keyboard", 21.99, "Computers"));
-            inventoryList.add(new Product("CP2154", "RGB Wireless Gaming Mouse", 67.45, "Computers"));
-            inventoryList.add(new Product("GM1032", "Telestrations Board Game", 17.99, "Games"));
-            inventoryList.add(new Product("GM1075", "Battleship Board Game", 12.99, "Games"));
-            inventoryList.add(new Product("GM1148", "Retro Handheld Arcade", 24.45, "Games"));
-            inventoryList.add(new Product("PW1255", "USB C to A Cable", 13.95, "Electronics"));
-            inventoryList.add(new Product("PW1341", "Solar Powered Battery Charger", 19.99, "Electronics"));
-            inventoryList.add(new Product("PW1431", "Wireless Charger iPhone", 12.99, "Electronics"));
-
             //Skip the header line
             scanner.nextLine();
 
